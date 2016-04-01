74588178cfc425603c3db590df0023648c554220
Tom Wilkinson <tbondwilkinson@gmail.com>
2016-04-01 13:36:06 -0500

    Initial commit

---
+++ BidirectionalMap/BidirectionalMap.h
@@ -0,0 +1,19 @@
+//
+//  BidirectionalMap.h
+//  BidirectionalMap
+//
+//  Created by Tom Wilkinson on 4/1/16.
+//  Copyright © 2016 Zefferus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BidirectionalMap.
+FOUNDATION_EXPORT double BidirectionalMapVersionNumber;
+
+//! Project version string for BidirectionalMap.
+FOUNDATION_EXPORT const unsigned char BidirectionalMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BidirectionalMap/PublicHeader.h>
+
+
